8879163fb797db94bae7e9314adbaf8aedcc6867
Ricardo Loo <ricardol@microsoft.com>
2016-01-13 16:45:48 -0800

    Updated header of the file pointing to license file in root of repo

--- android_app/app/src/main/java/com/microsoft/office365/meetingfeedback/MeetingDetailActivity.java
+++ android_app/app/src/main/java/com/microsoft/office365/meetingfeedback/MeetingDetailActivity.java
@@ -1,5 +1,6 @@
 /*
- *  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
  */
 package com.microsoft.office365.meetingfeedback;
 
